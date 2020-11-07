787417d1536355829bd836b9b3f86876dfe84954
vdmitriv15 <vdmitriv15@gmail.com>
2020-11-07 11:57:44 -0500

    DZ Lesson_2

--- task_7.py
+++ task_7.py
@@ -1,55 +1,8 @@
 """
-Задание 5.
-Задание на закрепление навыков работы с деком
+7.	Напишите программу, доказывающую или проверяющую, что для множества
+натуральных чисел выполняется равенство: 1+2+...+n = n(n+1)/2,
+ где n - любое натуральное число.
 
-В рассмотренном на уроке листинге есть один недостаток
-Приведенный код способен "обработать" только строку без пробелов, например, 'топот'
-
-Но могут быть и такие палиндромы, как 'молоко делили ледоколом'
-
-Вам нужно доработать программу так, чтобы она могла выполнить проверку на палиндром
-и в таких строках (включающих пробелы)
+ Решите через рекурсию. Решение через цикл не принимается.
+Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
-
-
-class DequeClass:
-    def __init__(self):
-        self.elems = []
-
-    def is_empty(self):
-        return self.elems == []
-
-    def add_to_front(self, elem):
-        self.elems.append(elem)
-
-    def add_to_rear(self, elem):
-        self.elems.insert(0, elem)
-
-    def remove_from_front(self):
-        return self.elems.pop()
-
-    def remove_from_rear(self):
-        return self.elems.pop(0)
-
-    def size(self):
-        return len(self.elems)
-
-
-def pal_checker(string):
-    dc_obj = DequeClass()
-    string = string.replace(' ', '')  # удаление пробелов
-    for el in string:
-        dc_obj.add_to_rear(el)
-
-    still_equal = True
-
-    while dc_obj.size() > 1 and still_equal:
-        first = dc_obj.remove_from_front()
-        last = dc_obj.remove_from_rear()
-        if first != last:
-            still_equal = False
-
-    return still_equal
-
-
-print(pal_checker("молоко делили ледоколом"))
